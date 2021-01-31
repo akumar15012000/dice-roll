3c79a9e66eaaf802ec71ad0fd889385348c27e52
Amit Kumar <61865137+akumar15012000@users.noreply.github.com>
2021-01-31 14:00:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var img="dice"+rand+".png";
 
 document.querySelectorAll("img")[0].src=img;
 
-var rand1=Math.floor((Math.random()*2))+5;
+var rand1=Math.floor((Math.random()*6))+1;
 
 var img1="dice"+rand1+".png";
 document.querySelectorAll("img")[1].src=img1;
